5de717fe0120ce7991d46fa1788254adf6a0175f
nickbedner <2321769+nickbedner@users.noreply.github.com>
2021-03-01 06:24:27 -0800

    Draw order sorting and window resizing

--- include/entities/entity.h
+++ include/entities/entity.h
@@ -10,10 +10,13 @@ struct Entity {
   unsigned int entity_id;
   vec3 position;
   float direction;
+  float width;
+  float height;
   void* entity_data;
   void (*delete_func)(void*, void*);
   void (*update_func)(void*, void*, float);
   void (*render_func)(void*, void*);
+  void (*recreate_func)(void*, void*);
 };
 
 #endif  // ENTITY_H
